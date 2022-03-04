942d84357ec26a55f03d656cd6658922e7ba086c
BeWorld-hub <Fenckoplet@yandex.ru>
2022-03-04 14:29:31 +0300

    Shell sort

---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <chrono>
+
+int main() {
+	int n{};
+	int vs{};
+	int reverse{};
+	std::cin >> n;
+	int* A = new int[n];
+
+	for (int i = 0; i < n; i++)
+		A[i] = std::rand();
+
+	int d = n / 2;
+	auto start_time = std::chrono::steady_clock::now();
+	while (d > 0) {
+		for (int i = 0; i < n - d; i++) {
+			int j = i;
+			while (j >= 0 && A[j] > A[j + d]) {
+				vs++;
+				std::swap(A[j], A[j + d]);
+				reverse++;
+				j--;
+
+			}
+			vs++;
+		}
+		d /= 2;
+	}
+
+	auto end_time = std::chrono::steady_clock::now();
+	auto work_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+	std::cout << "vs = " << vs << std::endl;
+	std::cout << "reverse = " << reverse << std::endl;
+	std::cout << "reverse + vs = " << reverse + vs << std::endl;
+	std::cout << "time(miliseconds): " << work_time.count() << std::endl;
+}
